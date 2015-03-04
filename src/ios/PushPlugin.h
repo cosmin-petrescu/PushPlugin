55bcfb34cb0eb7efeff87bd1a017261573904801
cosmin-petrescu <cosmin.petrescu@innobyte.com>
2015-03-04 12:01:25 +0200

    Update PushPlugin.h

--- src/ios/PushPlugin.h
+++ src/ios/PushPlugin.h
@@ -1,15 +1,5 @@
 /*
- Copyright 2009-2011 Urban Airship Inc. All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
- 
- 1. Redistributions of source code must retain the above copyright notice, this
- list of conditions and the following disclaimer.
- 
- 2. Redistributions in binaryform must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided withthe distribution.
+ Copyright 2015 SC Innobyte Solutions SRL. All rights reserved.
  
  THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
